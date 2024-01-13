cc7aff774b667cd91542689cceec9f003c828d42
Python Code <pycodebr@gmail.com>
2024-01-12 21:20:58 -0300

    Add upload file options on Genre admin

--- genres/admin.py
+++ genres/admin.py
@@ -1,7 +1,47 @@
+from django import forms
 from django.contrib import admin
+from django.shortcuts import render
+from django.urls import path
 from genres.models import Genre
+
+
+class FileUploadForm(forms.Form):
+    file = forms.FileField()
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
+    change_list_template = 'genre_changelist.html'
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('upload-file/', self.upload_file),
+        ]
+        return my_urls + urls
+
+    def upload_file(self, request):
+        if request.method == 'POST':
+            file = request.FILES['file']
+
+            # Processe seu arquivo aqui e faça o que quiser com ele (file)
+            print(f'Variável com conteúdo do arquivo: {file}')
+
+            form = FileUploadForm()
+            return render(
+                request=request,
+                template_name='admin/upload.html',
+                context={
+                    'form': form,
+                    'title': 'Importar arquivo',
+                    'response': 'Arquivo processado com sucesso.'
+                },
+            )
+
+        form = FileUploadForm()
+        return render(
+            request=request,
+            template_name='admin/upload.html',
+            context={'form': form, 'title': 'Importar arquivo'},
+        )
